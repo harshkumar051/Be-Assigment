0800d881146bba0ee3dcb3ecf497d569383049fa
Harshkumar051 <Hk180306@gmail.com>
2025-02-24 09:23:03 +0530

    assignment

---
+++ TASK 1/public/script.js
@@ -0,0 +1,17 @@
+let user=[
+    {
+        name: "John",
+        age: 30,
+        email: "Vikaskxbdkj@gmail.com"
+    },
+    {
+        name: "jjefhebf",
+        age: 23,
+        email: "Harshkxbdkj@gmail.com"   
+    },
+    {
+        name: "harsh",
+        age: 10,
+        email: "Vikaskxbdkj@gmail.com"
+    }
+]
